a0786bac8d4f6d2495beb08e5a888528704410f5
Remik <remigiuszszejna@gmail.com>
2023-05-20 23:41:03 +0200

    Add HibernateUtil

--- src/main/java/zajavka/pl/configuration/HibernateUtil.java
+++ src/main/java/zajavka/pl/configuration/HibernateUtil.java
@@ -1,68 +1,76 @@
 package zajavka.pl.configuration;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import zajavka.pl.domain.*;
 
 import java.util.Map;
 
 public class HibernateUtil {
 
     private static final Map<String, Object> SETTINGS =
-            Map.of(
-                    Environment.DRIVER, "org.postgreSql.Driver",
-                    Environment.URL, ""
-            )
+            Map.ofEntries(
+                    Map.entry(Environment.DRIVER, "org.postgresql.Driver"),
+                    Map.entry(Environment.URL, "jdbc:postgresql://localhost:5432/car_dealership"),
+                    Map.entry(Environment.USER, "postgres"),
+                    Map.entry(Environment.PASS, "postgres"),
+                    Map.entry(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect"),
+                    Map.entry(Environment.HBM2DDL_AUTO, "none"),
+                    Map.entry(Environment.SHOW_SQL, "true"),
+                    Map.entry(Environment.FORMAT_SQL, "true")
+            );
 
-        private static SessionFactory sessionFactory;
+    private static SessionFactory sessionFactory;
 
-        public static SessionFactory getSessionFactory() {
-            if (sessionFactory == null) {
-                try {
-                    Configuration configuration = new Configuration();
+    public static SessionFactory loadSessionFactory() {
+        try {
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(SETTINGS)
+                    .build();
 
-
-                    configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-                    configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/nazwa_bazy_danych");
-                    configuration.setProperty("hibernate.connection.username", "nazwa_użytkownika");
-                    configuration.setProperty("hibernate.connection.password", "hasło");
-
-                    // Konfiguracja dialektu bazy danych
-                    configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-
-                    // Dodaj klasy encji
-                    configuration.addAnnotatedClass(YourEntityClass.class);
-                    // Dodaj kolejne klasy encji, jeśli istnieją
-
-                    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                            .applySettings(configuration.getProperties());
-
-                    sessionFactory = configuration.buildSessionFactory(builder.build());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            return sessionFactory;
+            Metadata metadata = new MetadataSources(serviceRegistry)
+                    .addAnnotatedClass(Address.class)
+                    .addAnnotatedClass(CarServiceRequest.class)
+                    .addAnnotatedClass(CarToBuy.class)
+                    .addAnnotatedClass(CartoService.class)
+                    .addAnnotatedClass(Customer.class)
+                    .addAnnotatedClass(Invoice.class)
+                    .addAnnotatedClass(Mechanic.class)
+                    .addAnnotatedClass(Part.class)
+                    .addAnnotatedClass(Salesman.class)
+                    .addAnnotatedClass(Service.class)
+                    .addAnnotatedClass(ServiceMechanic.class)
+                    .addAnnotatedClass(ServicePart.class)
+                    .getMetadataBuilder()
+                    .build();
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
         }
     }
-    W powyższym przykładzie należy dostosować ustawienia połączenia do odpowiednich wartości dla twojej bazy danych, a także dodać klasy encji, które chcesz mapować za pomocą Hibernate.
 
-            Teraz, aby uzyskać instancję SessionFactory, można wywołać metodę getSessionFactory() z klasy HibernateConfig. Możesz użyć tej instancji do otwierania sesji Hibernate i wykonywania operacji na bazie danych.
+    static void closeSessionFactory() {
+        try {
+            sessionFactory.close();
+        } catch (Exception ex) {
+            System.err.println("Exception while closing SessionFactroy" + ex.getMessage());
+        }
+    }
 
-            java
-    Copy code
-    SessionFactory sessionFactory = HibernateConfig.getSessionFactory();
-    Session session = sessionFactory.openSession();
-
-// Wykonaj operacje na bazie danych
-
-session.close();
-    To jest podstawowa konfiguracja Hibernate bez użycia Springa w klasie Java. Pamiętaj, że musisz dodać odpowiednie zależności do swojego projektu i dostosować ustawienia do swojej konfiguracji bazy danych.
-
-
-
-
+    static Session getSession() {
+        try {
+            return sessionFactory.openSession();
+        } catch (Exception ex) {
+            System.err.println("Exception while opening session " + ex.getMessage());
+        }
+        return null;
+    }
+}
 
 
 
